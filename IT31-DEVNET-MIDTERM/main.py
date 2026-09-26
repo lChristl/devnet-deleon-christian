09cddaf0f51cea33a54800cd4fdb9ab4deb9a9ba
Christian F. De Leon <chris20170322@gmail.com>
2026-09-26 15:30:20 +0800

    started main function and call out display_menu

--- IT31-DEVNET-MIDTERM/main.py
+++ IT31-DEVNET-MIDTERM/main.py
@@ -2,17 +2,26 @@
 Midterm Practical Exam — Pet Adoption Records Manager
 Student: [De Leon, Christian F.]
 """
-
+pets =  []
 
 def display_menu():
     # print the menu, return the user's choice
-    pets =  []
     print("1. Add a pet")
     print("2. View all pets")
     print("3. Count available vs adopted")
-    print("4. Find  a pet by name")
+    print("4. Find a pet by name")
     print("5. Exit")
     option = input("Choose an option: ")
+    if option == 1:
+        print("")
+    elif option == 2:
+        print("")
+    elif option == 3:
+        print("")
+    elif option == 4:
+        print("")
+    elif option == 5:
+        print("")
     pass
 
 def add_pet(pet_list):
@@ -45,7 +54,16 @@ def remove_pet(pet_list):
 def main():
     running = True
     while running:
-        choice = display_menu()
-
+        print("[1] Display Menu")
+        print("[2] Exit")
+        choice = int(input("Insert number [1] or [2]: "))
+        if choice == 1:
+            display_menu()
+        elif choice == 2:
+            print("Exiting program")
+            break
+        else:
+            break
         # use if/elif to call the right function based on choice
         # set running = False when the user picks Exit
+main()
