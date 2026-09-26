a691253833a865add28005ff07ddce7492b53d4a
Christian F. De Leon <chris20170322@gmail.com>
2026-09-26 15:15:23 +0800

    moved my initial code inside display_menu

--- IT31-DEVNET-MIDTERM/main.py
+++ IT31-DEVNET-MIDTERM/main.py
@@ -2,30 +2,35 @@
 Midterm Practical Exam — Pet Adoption Records Manager
 Student: [De Leon, Christian F.]
 """
-pets =  []
 
-print("1. Add a pet")
-print("2. View all pets")
-print("3. Count available vs adopted")
-print("4. Find  a pet by name")
-print("5. Exit")
-
-option = input("Choose an option: ")
 
 def display_menu():
     # print the menu, return the user's choice
+    pets =  []
+    print("1. Add a pet")
+    print("2. View all pets")
+    print("3. Count available vs adopted")
+    print("4. Find  a pet by name")
+    print("5. Exit")
+    option = input("Choose an option: ")
     pass
 
 def add_pet(pet_list):
     # ask for name, animal type, status — build the string, add to the list
+    p_name = input("What is your pet's name?: ")
+    p_type = input("What animal type is your pet?: ")
+    p_status = input("Is the pet Available or Adopted?")
     pass
 
 def view_pets(pet_list):
     # loop through and print every pet — handle empty list
+    for x in pets:
+        print(x)
     pass
 
 def count_available_adopted(pet_list):
     # loop through, count Available vs Adopted, return both
+
     pass
 
 def find_pet(pet_list):
@@ -41,5 +46,6 @@ def main():
     running = True
     while running:
         choice = display_menu()
+
         # use if/elif to call the right function based on choice
         # set running = False when the user picks Exit
