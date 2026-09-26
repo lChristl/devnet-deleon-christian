300566a27871d54cdb476efb49179febdd81d410
Christian F. De Leon <chris20170322@gmail.com>
2026-09-26 16:19:31 +0800

    add remove_pet function

--- IT31-DEVNET-MIDTERM/main.py
+++ IT31-DEVNET-MIDTERM/main.py
@@ -6,23 +6,28 @@ pet_list =  []
 
 def display_menu():
     # print the menu, return the user's choice
-    print("1. Add a pet")
-    print("2. View all pets")
-    print("3. Count available vs adopted")
-    print("4. Find a pet by name")
-    print("5. Exit")
-    option = int(input("Choose an option: "))
-    if option == 1:
-        add_pet(pet_list)
-    elif option == 2:
-        view_pets(pet_list)
-    elif option == 3:
-        count_available_adopted(pet_list)
-    elif option == 4:
-        find_pet(pet_list)
-    elif option == 5:
-        print("")
-    pass
+    try:
+        print("1. Add a pet")
+        print("2. View all pets")
+        print("3. Count available vs adopted")
+        print("4. Find a pet by name")
+        print("5. Remove a pet")
+        print("6. Exit")
+        option = int(input("Choose an option: "))
+        if option == 1:
+            add_pet(pet_list)
+        elif option == 2:
+            view_pets(pet_list)
+        elif option == 3:
+            count_available_adopted(pet_list)
+        elif option == 4:
+            find_pet(pet_list)
+        elif option == 5:
+            remove_pet(pet_list)
+        elif option == 6:
+            print("Exiting...")
+    except:
+        print("Insert number values only from [1] - [6]")
 
 def add_pet(pet_list):
     # ask for name, animal type, status — build the string, add to the list
@@ -59,6 +64,8 @@ def find_pet(pet_list):
 # BONUS (optional)
 def remove_pet(pet_list):
     # your code here
+    rm = input("Remove pet")
+    pet_list.remove(rm)
     pass
 
 def main():
@@ -71,7 +78,7 @@ def main():
             display_menu()
         elif choice == 2:
             running = False
-            print("Ending program")
+            print("Ending program...")
         else:
             break
         # use if/elif to call the right function based on choice
